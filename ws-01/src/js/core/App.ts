f8e290158205a74c3e1063fff6b6f34a74a64093
Edu Prats <eduprats@gmail.com>
2024-11-29 13:16:36 +0100

    GSAP animations intro

--- ws-01/src/js/core/App.ts
+++ ws-01/src/js/core/App.ts
@@ -2,6 +2,9 @@ import { ThreeDOMLayer } from "@fils/gl-dom";
 import { ThreeSketch } from "../gfx/ThreeSketch";
 import { Timer } from "@fils/ani";
 import { D, Scroller } from "@fils/scroller";
+import { HeaderSection } from "../sections/HeaderSection";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
 
 export class App {
 	gl:ThreeDOMLayer;
@@ -13,7 +16,8 @@ export class App {
 		this.gl = new ThreeDOMLayer(document.querySelector('.view'));
 		this.gl.renderer.setClearColor(0xffffff, 1);
 		this.sketch = new ThreeSketch(this.gl);
-		this.start();
+
+		gsap.registerPlugin(SplitText);
 
 		this.scroller = new Scroller({
 			// allowVerticalScrolling: false,
@@ -21,6 +25,30 @@ export class App {
 			easing: 0.2,
 			// snapping: true
 		});
+
+		// custom sections
+		const sections = document.querySelectorAll('[fil-scroller-section="hello"]');
+		for(const s of sections) {
+			//@ts-ignore
+			const cs = new HeaderSection(s, this.scroller);
+		}
+
+		const container = document.querySelector('[fil-scroller]');
+		const rs = new ResizeObserver(() => {
+			// console.log('resize');
+			this.scroller.restore();
+		})
+		rs.observe(container);
+
+		gsap.ticker.remove(gsap.updateRoot);
+
+		this.start();
+
+		// rs.disconnect();
+
+		/* window.addEventListener('resize', event => {
+			this.scroller.restore();
+		}); */
 	}
 
 	start() {
@@ -32,11 +60,23 @@ export class App {
 		requestAnimationFrame(animate);
 
 		this.clock = new Timer(true);
+
+		window.addEventListener('blur', event => {
+			console.log('blur');
+			this.clock.pause();
+		})
+
+		window.addEventListener('focus', event => {
+			console.log('focus');
+			this.clock.resume();
+		})
 	}
 
 	update() {
+		if(this.clock.paused) return;
 		this.clock.tick();
 		const t = this.clock.currentTime;
+		gsap.updateRoot(t);
 		this.sketch.update(t);
 		this.sketch.render();
 		this.scroller.update();
